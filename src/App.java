0c948189f4100631688e88c396219c9ca5a04804
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2024-06-05 09:59:55 -0300

    🐛 Fixed bug in getInstance of MainView

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import bank.view.View;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        View view = MainView.getInstance();
+        View view = new MainView();
         view.startView();
     }
 }
